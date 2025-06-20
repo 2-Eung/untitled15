8abd53a756f4927bb7cf1ff8532bc9655324c3ee
2-Eung <eung4256@gmail.com>
2025-06-20 13:03:46 +0900

    Thread extends Inheritance

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,26 @@
-import java.util.Arrays;
-import java.util.List;
-
+class MyThread extends Thread {
+    @Override
+    public void run () {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("My Thread : " + i);
+//            Thread.sleep(5000);            // 에러 처리를 안해서 이것만으로는 불가
+            try {
+                Thread.sleep(5000);     // 에러 처리해서 가능해짐
+            } catch (InterruptedException e) {      // 에러는 언제 날까???
+                System.out.println(e.getMessage()); // sleep 하고 있을때 누가 깨우면 안되서
+            }
+        }
+    }
+}
 public class Main {
 
     public static void main(String[] args) {
-        List<Double> values = Arrays.asList(10.0, 20.0, 30.0);
+        MyThread thread = new MyThread();
+//       thread.run();       // .run(); 으로 실행하면 일반 메소드로 실행이 됨 일반 메소드는
+                            // 메인 Thread 안에서 하겠다는거니까 Thread 만든 의미가 없음
+        thread.start();     // 다른 Thread (우리가 만든거) 로 하려면 .start(); 로 해야됨
 
-        double avg = values.stream()
-                .mapToDouble(Double::doubleValue)
-                .average()
-                .orElse(0.0);   // 안전장치 : 값이 없을 경우 0 으로 나눠버릴수 있어서
-                                      // 0.0 은 기본값
-        System.out.println(avg);
+
 
     }
 }
