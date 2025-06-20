976f5b7e1085795e9a51ef54ceca88249a9fc0cd
2-Eung <eung4256@gmail.com>
2025-06-20 13:24:31 +0900

    Thread implements Runnable threadNaming

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
-class MyThread extends Thread {
-    @Override
-    public void run () {
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("My Thread : " + i);
-//            Thread.sleep(5000);            // 에러 처리를 안해서 이것만으로는 불가
+class MyRunnable implements Runnable {  // Runnable 을 implements 시 빨간줄이 뜨는데
+    @Override                           // Runnable 내의 추상 메소드인 run(); 때문이다.
+    public void run() {                 // 그래서 run(); 을 오버라이딩 해주면 없어진다.
+        System.out.println("Runnable 실행중");
+
+        for (int i = 1; i<=5; i++) {
             try {
-                Thread.sleep(5000);     // 에러 처리해서 가능해짐
-            } catch (InterruptedException e) {      // 에러는 언제 날까???
-                System.out.println(e.getMessage()); // sleep 하고 있을때 누가 깨우면 안되서
+                Thread.sleep(5000);
+                System.out.println(i);
+            } catch (InterruptedException e) {
+                System.out.println(e.getMessage());
             }
         }
     }
@@ -15,12 +16,12 @@ class MyThread extends Thread {
 public class Main {
 
     public static void main(String[] args) {
-        MyThread thread = new MyThread();
-//       thread.run();       // .run(); 으로 실행하면 일반 메소드로 실행이 됨 일반 메소드는
-                            // 메인 Thread 안에서 하겠다는거니까 Thread 만든 의미가 없음
-        thread.start();     // 다른 Thread (우리가 만든거) 로 하려면 .start(); 로 해야됨
-
-
+        Thread thread = new Thread(new MyRunnable(), "CountThread"); // thread 에다가 우리가 만든 MyRunnable() 을 집어넣는다.
+                                                                           // 인스턴스 선언시 이름지을수도 있다.
+        thread.setName("고장없이 잘 돌아가는 최신 쓰레드");  // 세터, 이름을 정하는 세터
+                                                        // thread 0 -> ~~~
+        thread.start();
+        System.out.println(thread.getName());           // 게터
 
     }
 }
